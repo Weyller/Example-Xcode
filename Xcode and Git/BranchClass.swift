10e0324a0500b1fd473f8f4aa0cbab5bc2e30a9b
eleves <eleves@mac404inf-09.local>
2017-07-03 09:24:36 -0400

    create class for new branch

---
+++ Xcode and Git/BranchClass.swift
@@ -0,0 +1,16 @@
+//
+//  BranchClass.swift
+//  Xcode and Git
+//
+//  Created by eleves on 2017-07-03.
+//  Copyright © 2017 eleves. All rights reserved.
+//
+
+import Foundation
+
+class BranchClass
+{
+    
+    var test = "test"
+    
+}
--- Xcode and Git/ViewController.swift
+++ Xcode and Git/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var testPrint = BranchClass()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(testPrint.test)
+        
     }
 
     override func didReceiveMemoryWarning() {
